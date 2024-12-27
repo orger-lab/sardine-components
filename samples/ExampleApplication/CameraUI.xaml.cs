e7e86788a37149e8b8fe1f2d4ce3c311dbe2d793
saco.zarco <saco.zarco@utopia-society.com>
2024-12-27 17:51:52 +0000

    Added example SARDINE.xml and a 'break' method to simulate a malfunction

--- samples/ExampleApplication/CameraUI.xaml.cs
+++ samples/ExampleApplication/CameraUI.xaml.cs
@@ -23,6 +23,7 @@ namespace ExampleApplication
                     Handle?.Start();
             }
         }
+
     }
 
 }
---
+++ samples/ExampleApplication/CameraUIThrowException.cs
@@ -0,0 +1,22 @@
+﻿using ExampleSystem;
+using Sardine.Core.Views.WPF;
+using System.Windows;
+
+namespace ExampleApplication
+{
+    /// <summary>
+    /// Interaction logic for CameraBreakingUI.xaml
+    /// </summary>
+    public partial class CameraUIThrowException : VesselUserControl<Camera>
+    {
+        public CameraUIThrowException()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Break_Click(object sender, RoutedEventArgs e)
+        {
+            Handle?.BreakCamera();
+        }
+    }
+}
